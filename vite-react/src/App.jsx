1bfc5950a63af14ae98b922b46bd5f11c989e342
diya goyal <diya.goyal1607@gmail.com>
2026-02-12 22:20:18 +0530

    counnter done

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,10 +1,33 @@
- 
-function App() {
-  
+ import { useState } from "react";
 
+function App() {
+
+  const [count, setCount] = useState(0);
+
+    const increase = () => {
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  };
+
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
   return (
-     <h1>Vite + React</h1>
-  )
+    <>
+      <h1>COUNTER : {count}</h1>
+
+      <button onClick={ increase}>
+        Add value
+      </button>
+
+      <button onClick={ decrease}>
+        Remove value
+      </button>
+    </>
+  );
 }
 
-export default App
+export default App;
