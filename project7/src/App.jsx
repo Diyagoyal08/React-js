db9ca8100ae7925dd315749754d7da449028885b
diya goyal <diya.goyal1607@gmail.com>
2026-02-19 23:56:19 +0530

    react router done

--- project7/src/App.jsx
+++ project7/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+ 
+import Header from './components/Header/header';
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-600 p-4'>React Router</h1>
+     
+      
     </>
   )
 }
