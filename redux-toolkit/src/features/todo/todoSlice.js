3d19dbf6fd3e868acb375d7ebae9eb3f53af1f1b
diya goyal <diya.goyal1607@gmail.com>
2026-02-23 22:06:46 +0530

    createslice

---
+++ redux-toolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,22 @@
+import { createSlice  , nanoid} from "@reduxjs/toolkit";
+
+const initialState = {
+    todos : [{id: nanoid() , title : "Learn Redux Toolkit" , completed : false}]
+}
+
+export const todoSlice = createSlice({
+    name : "todos",
+    initialState,
+    reducers : {
+        addTodo : (state , action) => {
+            const todo = {
+                id : nanoid(),
+                title : action.payload,
+                completed : false
+            }
+            state.todos.push(todo)
+        },
+        removeTodo : (state , action) => {
+            state.todos = state.todos.filter(todo => todo.id !== action.payload)
+        },
+})
