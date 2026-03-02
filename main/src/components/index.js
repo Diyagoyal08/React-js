a54637b5ba939cac4bb4b6e71ec35071bb2b34a8
diya goyal <diya.goyal1607@gmail.com>
2026-03-02 10:53:14 +0530

    components

--- main/src/components/index.js
+++ main/src/components/index.js
@@ -1,7 +1,11 @@
 import Header from "./header/header";
 import Footer from "./Footer/Footer";
+import container from "./container/container";
+import Logo from "./Logo";
 
 export { 
     Header,
-    Footer 
+    Footer,
+    container,
+    Logo,
 };
